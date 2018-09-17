3185b6c892613a1d141d05f9f101f55a2e8099de
ali iqbali <ali.iqbali@frontedgeit.se>
2018-09-17 09:00:25 +0200

    ES6 Strings

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,4 @@
-/////////////////////
+/////////////////////////////////
 // let and const
 
 // //ES5
@@ -60,7 +60,7 @@
 // console.log(i);
 
 
-/////////////////////
+/////////////////////////////////
 // Blocks and IIFEs
 
 //ES6
@@ -80,11 +80,28 @@
 // console.log(c);
 
 
+/////////////////////////////////
+// Strings
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+function calcAge(year) {
+    return 2016 - year;
+}
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. today he is ' + calcAge(yearOfBirth) + ' year old.');
+
+// ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today he is ${calcAge(yearOfBirth)} years old.`);
 
 
-
-
-
+const n = `${ firstName } ${ lastName }`;
+console.log(n.startsWith('j'));
+console.log(n.endsWith('Sm'));
+console.log(n.includes('oh'));
+console.log(`${firstName} `.repeat(5));
 
 
 
