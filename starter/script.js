3cd2ba3b0e696ba08005b1d8573229d424edb609
ali iqbali <ali.iqbali@frontedgeit.se>
2018-09-17 10:05:53 +0200

    Destruturing

--- starter/script.js
+++ starter/script.js
@@ -135,33 +135,33 @@
 // Arrow functions part 2
 
 // ES5
-var box5 = {
-    color: 'green',
-    position: 1,
-    clickMe: function () {
+// var box5 = {
+//     color: 'green',
+//     position: 1,
+//     clickMe: function () {
 
-        var self = this;
-        document.querySelector('.green').addEventListener('click', function () {
-            var str = 'This is box number ' + self.position + ' and its is ' + self.color;
-            alert(str);
-        })
-    }
-}
-// box5.clickMe();
+//         var self = this;
+//         document.querySelector('.green').addEventListener('click', function () {
+//             var str = 'This is box number ' + self.position + ' and its is ' + self.color;
+//             alert(str);
+//         })
+//     }
+// }
+// // box5.clickMe();
 
 
-// ES6
-const box6 = {
-    color: 'green',
-    position: 1,
-    clickMe: function () {
+// // ES6
+// const box6 = {
+//     color: 'green',
+//     position: 1,
+//     clickMe: function () {
 
-        document.querySelector('.green').addEventListener('click', () => {
-            var str = 'This is box number ' + this.position + ' and its is ' + this.color;
-            alert(str);
-        })
-    }
-}
+//         document.querySelector('.green').addEventListener('click', () => {
+//             var str = 'This is box number ' + this.position + ' and its is ' + this.color;
+//             alert(str);
+//         })
+//     }
+// }
 // box6.clickMe();
 /*
 const box66 = {
@@ -178,46 +178,70 @@ const box66 = {
 box66.clickMe();
 */
 
+// // ES5
+// function Person(name) {
+//     this.name = name;
+// }
+
+// Person.prototype.myFriends5 = function (friends) {
+
+//     var arr = friends.map(function (el) {
+//         return this.name + ' is friends with ' + el;
+
+//     }.bind(this));
+//     console.log(arr);
+// }
+// var friends = ['Bob', 'Jane', 'Mark'];
+// new Person('John').myFriends5(friends);
+
+// // ES6
+// function Person(name) {
+//     this.name = name;
+// }
+
+// Person.prototype.myFriends6 = function (friends) {
+
+//     var arr = friends.map(el => `${this.name} is friends with ${el}`);
+//     console.log(arr);
+// }
+
+// new Person('Mike').myFriends6(friends);
+
+
+/////////////////////////////////
+// Destructuring
+
 // ES5
-function Person(name) {
-    this.name = name;
-}
+var john = ['john', 26];
+// var name = john[0];
+// var age = john[1];
 
-Person.prototype.myFriends5 = function (friends) {
-
-    var arr = friends.map(function (el) {
-        return this.name + ' is friends with ' + el;
-
-    }.bind(this));
-    console.log(arr);
-}
-var friends = ['Bob', 'Jane', 'Mark'];
-new Person('John').myFriends5(friends);
 
 // ES6
-function Person(name) {
-    this.name = name;
+const [name, age] = ['john', 26];
+console.log(name);
+console.log(age);
+
+const obj = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+const { firstName, lastName } = obj;
+console.log(firstName);
+console.log(lastName);
+
+const { firstName: a, lastName: b } = obj;
+console.log(a);
+console.log(b);
+
+function calcAgeRetriment(year) {
+    const age = new Date().getFullYear() - year;
+    return [age, 65 - age];
 }
 
-Person.prototype.myFriends6 = function (friends) {
-
-    var arr = friends.map(el => `${this.name} is friends with ${el}`);
-    console.log(arr);
-}
-
-new Person('Mike').myFriends6(friends);
-
-
-
-
-
-
-
-
-
-
-
-
+const[age2, retirement]= calcAgeRetriment(1990);
+console.log(age2);
+console.log(retirement);
 
 
 
